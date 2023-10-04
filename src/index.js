040ecf700e86686d5cc24e358f230053821d4c09
numbernine-09 <111489517+numbernine-09@users.noreply.github.com>
2023-10-04 20:40:07 +0700

    remove unuse method

--- src/index.js
+++ src/index.js
@@ -10,14 +10,6 @@ app.use(express.json()); // belum dibutuhkan
 app.use(express.urlencoded({ extended: true })); // belum dibutuhkan
 app.use(morgan("tiny"));
 
-app.get("/api", (req, res) => {
-  try {
-    res.status(200).send("Selamat datang di API FILM by Rangga");
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
-
 app.use("/films", filmRoutes);
 
 app.listen(PORT, () => {
